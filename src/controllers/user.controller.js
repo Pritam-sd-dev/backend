d328c0cf737c1ec523d1f27a69242fca36269b4d
pritam <pritam.pk36129@gmail.com>
2024-06-04 20:08:27 +0530

    add pagination in timeline api

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -50,13 +50,41 @@ export const login = async (req, res) => {
 }
 
 export const getUserTimeline = async (req, res) => {
-    const {userId} = req.params;
-
+    const { cursor, limit = 10 } = req.query;
+    const { userId } = req.params
     // doubt
-    console.log(userId);
+
     try {
-        const tweets = await Tweet.find({userId}).select('-userId');
-        res.status(200).json({status: true, message: '', data: {tweets}});
+        let query = {userId};
+
+        // If there is a cursor, use it to fetch results after this cursor
+        if (cursor) {
+            query._id = { $gt: cursor };
+        }
+
+        const tweets = await Tweet.find(query).select('-userId')
+        // .sort({ _id: -1 }) // Sort in descending order of _id (most recent first)
+        .limit(Number(limit) + 1);
+
+        let hasNextPage = tweets.length > Number(limit);
+        if (hasNextPage) {
+            // Remove the extra element used to determine next page
+            tweets.pop();
+        }
+
+
+        res.status(200).json(
+            {
+                status: true,
+                message: '', 
+                data: {
+                    tweets,
+                    hasNextPage,
+                    nextCursor: hasNextPage ? tweets[tweets.length - 1] : null,
+                }
+            }
+        );
+
     } catch (error) {
         res.status(500).json({status: false, message: 'something went wrong :('});
     }
