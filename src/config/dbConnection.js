fb95f7052b91c69943ccde29c04dc5f84e2389f3
pritam <pritam.pk36129@gmail.com>
2024-06-12 21:33:19 +0530

    add db url in .env

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose"
 
 mongoose.set('strictQuery', false);
-const mongo_uri = 'mongodb://localhost:27017';
+
 const connectToDB = async () => {
     console.log(process.env.DATABASE_PASSWORD);
-    const {connection} = await mongoose.connect(mongo_uri);
+    const {connection} = await mongoose.connect(process.env.DB_URL);
 
     if(connection) {
         console.log(`Database connected at ${connection.host}`);
