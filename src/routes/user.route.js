bdf02d329735c1613728935a1397df493ef38214
pritam <pritam.pk36129@gmail.com>
2024-06-16 10:30:47 +0530

    add auth middleware in timeline api

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { getUserTimeline, login, reqisterUser } from "../controllers/user.controller.js";
+import isLoggedIn from "../middlewares/authentication.js";
 
 const router = Router();
 
 router.post('/register', reqisterUser);
 router.post('/login', login);
-router.get('/:userId/timeline', getUserTimeline);
+router.get('/:userId/timeline', isLoggedIn, getUserTimeline);
 
 
 export default router;
